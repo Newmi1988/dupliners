47b4ca7224b15dc316f6137c47341281e5f5aeab
Tobias Newmiwaka <newmi1988@gmail.com>
2022-08-05 22:39:26 +0200

    added pruning for hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,8 @@
-use sha2::{Digest, Sha256};
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::{BufRead, BufReader},
-    path::Path,
-};
+use std::path::Path;
 
-struct FileDuplicates {
-    dupes: HashMap<String, Vec<u32>>,
-}
+use crate::file_duplicates::FileDuplicates;
 
-fn hash_string(line: &String) -> Result<String, Box<dyn std::error::Error>> {
-    let mut hasher = Sha256::new();
-    // write input message
-    hasher.update(line.as_bytes());
-    // read hash digest and consume hasher
-    Ok(format!("{:X}", hasher.finalize()))
-}
-
-impl FileDuplicates {
-    fn new() -> FileDuplicates {
-        FileDuplicates {
-            dupes: HashMap::new(),
-        }
-    }
-
-    fn add(&mut self, line: &String, number: u32) -> () {
-        let hash = hash_string(line).expect("Error hashing line");
-        self.dupes.entry(hash).or_insert(Vec::new()).push(number);
-    }
-
-    fn from_file(&mut self, filepath: &Path) -> Result<(), Box<dyn std::error::Error>> {
-        let file = File::open(filepath)?;
-        let reader = BufReader::new(file);
-
-        Ok(for line in reader.lines() {
-            self.add(&line?, 12);
-        })
-    }
-}
-
-impl Default for FileDuplicates {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+mod file_duplicates;
 
 fn main() {
     let mut t = FileDuplicates::default();
@@ -54,20 +12,8 @@ fn main() {
     // t.add(&line, 1);
     let f = Path::new("./README.md");
     t.from_file(&f);
-
+    t.prune();
     print!("{:?}", t.dupes);
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn test_hash_string() {
-        let s = "Test".to_string();
-        assert_eq!(
-            hash_string(&s).expect("Error"),
-            "532EAABD9574880DBF76B9B8CC00832C20A6EC113D682299550D7A6E0F345E25".to_string()
-        )
-    }
 }
