a596cd5ad77999e900074c53b48cd2dbe94d536e
Tobias Newmiwaka <newmi1988@gmail.com>
2022-11-08 20:55:23 +0100

    Added cli to programm

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ use crate::{file_duplicates::FileDuplicates, folder_explorer::visit_dirs};
 
 mod file_duplicates;
 mod folder_explorer;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short,long, default_value_t = String::from("./src"))]
+    path: String,
+}
 
 fn main() {
+    let args = Args::parse();
     let mut t = FileDuplicates::default();
 
-    // let line: String = "Test".to_string();
-    let root = Path::new("./src");
+    let root = Path::new(&args.path);
 
     t.recurse_fs(root).expect("IO Error");
     t.prune();
